db0d757f5ee14db738c3d2f63e4a4e806cc67d5d
devedux <devedux@gmail.com>
2021-10-06 00:49:37 +0000

    validations for vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,8 @@
 class Vote < ApplicationRecord
+  # Assocations
   belongs_to :user
-  belongs_to :movie
+  belongs_to :movie, counter_cache: :votes_count
+
+  # Validations
+  validates :user, uniqueness: { scope: :movie, message: 'and Movie combination already taken' }
 end
