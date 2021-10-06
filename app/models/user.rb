92c4daaea24663dd6c96da253af3ae04698210c4
devedux <devedux@gmail.com>
2021-10-06 00:13:43 +0000

    Add validations for user Movel

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  # Constants
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
+  # Enums
+  enum role: { member: 0, actor: 1, director: 2, admin: 3 }
+
+  # Associations
+
+  # Validations
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+  validates :email, uniqueness: true
+  validates :email, presence: true
+  validates :username, uniqueness: true
+  validates :username, presence: true
 end
