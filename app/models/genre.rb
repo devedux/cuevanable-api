0f5d7e838fba5782b6d7bf13a14ad5c4669d2bed
devedux <devedux@gmail.com>
2021-10-06 00:33:50 +0000

    Add validation for genre model

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,8 @@
 class Genre < ApplicationRecord
+  # Associations
+  has_and_belongs_to_many :movies
+
+  # Validations
+  validates :name, presence: true
+  validates :name, length: { minimun: 5 }
 end
