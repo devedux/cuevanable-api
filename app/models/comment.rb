f09750cf368dd527f8dbc4948a4f8651593fa973
devedux <devedux@gmail.com>
2021-10-06 00:16:39 +0000

    Add validations for comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,22 @@
 class Comment < ApplicationRecord
+  # Enums
+  enum category: { reply: 0, comment: 1 }
+
+  # Associations
   belongs_to :user
   belongs_to :movie
+
+  # Validations
+  validates :body, presence: true
+  validates :body, length: { maximun: 150 }
+
+  validate :set_tweet_or_reply_category
+
+  def set_tweet_or_reply_category
+    self.category = if replied_to.present?
+                      0
+                    else
+                      1
+                    end
+  end
 end
--- scripts/git-config.bash
+++ scripts/git-config.bash
@@ -3,4 +3,5 @@
 echo "Configuring git..."
 git config --global user.email $GIT_USER_EMAIL
 git config --global user.name $GIT_USER_NAME
+git config advice.ignoredHook false
 echo "Done!"
